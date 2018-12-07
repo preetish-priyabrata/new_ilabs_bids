4eae1bb60591e4cb9bb1ef8eb48317a07e161bf9
preetish-priyabrata <ppriyabrata8888@gmail.com>
2018-12-07 12:10:36 +0530

    removing tag query and view bid information

--- application/views/vendors_user/vendor_template/v_template_top_menu.php
+++ application/views/vendors_user/vendor_template/v_template_top_menu.php
@@ -1,40 +1,22 @@
 <!-- begin #top-menu -->
 		<div id="top-menu" class="top-menu" >
-            <!-- begin top-menu nav -->
+			<!-- begin top-menu nav -->
 			<ul class="nav">
 				<li>
 					<a href="<?=base_url()?>user-vendor-new-technical">
-						
 						<div class="icon-img">
 							<img src="<?=base_url()?>file_css_admin/assets/img/logo/logo-bs4.png" alt="" />
 						</div>
-						<span>Technical Bid<span class="label label-theme m-l-5">NEW</span></span> 
+						<span>Technical Bid<span class="label label-theme m-l-5">NEW</span></span>
 					</a>
 				</li>
-				<li>
-					<a href="<?=base_url()?>user-vendor-query-panel">
-						<div class="icon-img">
-							<img src="<?=base_url()?>file_css_admin/assets/img/logo/logo-bs4.png" alt="" />
-						</div>
-						<span>Query<span class="label label-theme m-l-5">Panel</span></span> 
-				</a>
+				<li class="menu-control menu-control-left">
+					<a href="javascript:;" data-click="prev-menu"><i class="fa fa-angle-left"></i></a>
 				</li>
-				<li>
-					<a href="<?=base_url()?>user-vendor-view-details">
-						<div class="icon-img">
-							<img src="<?=base_url()?>file_css_admin/assets/img/logo/logo-bs4.png" alt="" />
-						</div>
-						<span>View Details<span class="label label-theme m-l-5">Info</span></span> 
-				</a>
+				<li class="menu-control menu-control-right">
+					<a href="javascript:;" data-click="next-menu"><i class="fa fa-angle-right"></i></a>
 				</li>
-                <li class="menu-control menu-control-left">
-                    <a href="javascript:;" data-click="prev-menu"><i class="fa fa-angle-left"></i></a>
-                </li>
-                <li class="menu-control menu-control-right">
-                    <a href="javascript:;" data-click="next-menu"><i class="fa fa-angle-right"></i></a>
-                </li>
-            </ul>
-            <!-- end top-menu nav -->
-        </div>
+			</ul>
+			<!-- end top-menu nav -->
+		</div>
 		<!-- end #top-menu -->
-		
